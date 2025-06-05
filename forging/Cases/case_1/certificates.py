67d06cdadf89056c404c2f16f3334f6e518c9ed9
Fabio Plunser <fabio.plunser@student.uibk.ac.at>
2025-06-05 08:06:29 +0200

    Unpacked cases

---
+++ forging/Cases/case_1/certificates.py
@@ -0,0 +1,86 @@
+from Crypto.Cipher import AES
+import json
+from Crypto.Hash import SHA256
+from dsa import *
+
+
+def gen_keys():
+    signature_scheme = DSA()
+    signature_scheme.keygen()
+    public_key = { 
+        'q': signature_scheme.q,
+        'p': signature_scheme.p,
+        'g': signature_scheme.g,
+        'y': signature_scheme.y
+    }   
+
+    private_key = { 
+        'q': signature_scheme.q,
+        'p': signature_scheme.p,
+        'g': signature_scheme.g,
+        'x': signature_scheme.x
+    }  
+
+    with open('public_key.txt', 'w') as pk:
+        pk.write(json.dumps(public_key))
+        pk.close()
+
+    with open('private_key.txt', 'w') as sk:
+        sk.write(json.dumps(private_key))
+        sk.close()
+
+    return signature_scheme
+
+def gen_certificate(username:str, website_name:str, expiration_date:str):
+    cert = {
+        'name': username,
+        'website': website_name,
+        'expdate': expiration_date
+    }
+    return json.dumps(cert)
+
+
+def sign_certificate_v1(signature_scheme: DSA, certificate:str, nonce):
+    cert = json.loads(certificate)
+    r,s = signature_scheme.sign(certificate.encode(), nonce, lambda m: hash(m))
+    cert['r'] = r
+    cert['s'] = s
+
+    return json.dumps(cert) 
+
+
+
+def hash(message):
+    h = SHA256.new(message).digest()
+    return h
+
+
+
+
+sig_scheme = gen_keys()
+nonce = random.randint(2, sig_scheme.q-1)
+while GCD(nonce, sig_scheme.q) != 1:
+    nonce = random.randint(2, sig_scheme.q-1)
+
+cert1 = gen_certificate('lucacampa', 'https://project3topic1.com', '01/01/2026')
+cert1 = sign_certificate_v1(sig_scheme, cert1, nonce)
+
+cert2 = gen_certificate('lucacampa', 'https://project2topic2.com', '01/01/2027')
+cert2 = sign_certificate_v1(sig_scheme, cert2, nonce)
+
+c1 = json.loads(cert1)
+c2 = json.loads(cert2)
+
+r1, s1 = c1['r'], c1['s']
+r2, s2 = c2['r'], c2['s']
+
+  
+
+
+with open('certificate_1.txt', 'w') as file:
+    file.write(cert1)
+    file.close()
+
+with open('certificate_2.txt', 'w') as file:
+    file.write(cert2)
+    file.close()
---
+++ forging/Cases/case_3/dsa.py
@@ -0,0 +1,71 @@
+import random
+from Crypto.Util.number import getPrime, GCD, bytes_to_long, isPrime
+
+
+
+
+#### Basic implementation
+class DSA:
+    """
+    It generates the necessary parameters
+    """
+    def __init__(self): 
+        self.p = 0
+        while not isPrime(self.p):
+            self.q = getPrime(128)
+            self.p = 2*self.q + 1
+
+        self.h = 2
+        self.g = pow(self.h, (self.p-1)//self.q, self.p)
+
+        while self.g == 1: 
+            self.h = random.randint(2, self.p-2)
+            self.g = pow(self.h, (self.p-1)//self.q, self.p)
+    
+
+
+    """
+    It generates the public and private key
+    """
+    def keygen(self):
+        self.x = random.randint(1, self.q-1) # private key
+        self.y = pow(self.g, self.x, self.p) # public key
+        
+
+    """
+    It signs a message
+    """
+    def sign(self, message, k,  f):
+
+        assert GCD(k, self.q) == 1
+
+        r = pow(self.g, k, self.p) % self.q
+
+        hashed_message = bytes_to_long(f(bytes.fromhex(message['name']+message['website']+message['expdate'])))
+
+        s = ((hashed_message + self.x*r)*pow(k, -1, self.q)) % (self.q)
+
+       
+        assert s != 0
+        return r, s
+
+    """
+    It verifies a message
+    """
+    def verify(self, message, signature, f):
+
+        r, s = signature
+        assert s != 0
+        
+        hashed_message = bytes_to_long(f(bytes.fromhex(message['name']+message['website']+message['expdate'])))
+
+        temp = pow(s, -1, self.q)
+
+        eq1 = (hashed_message*temp) % self.q
+        eq2 = (r*temp) % self.q
+
+        v = ((pow(self.g, eq1, self.p)*pow(self.y, eq2, self.p)) % self.p) % self.q
+
+        return v == r
+        
+
